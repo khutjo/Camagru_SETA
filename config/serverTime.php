da929e8a84e79da3ac0cb52450e125496d28829b
khutjo <kmaputla@student.wethinkcode.co.za>
2020-08-27 21:41:27 +0200

    camera

--- config/serverTime.php
+++ config/serverTime.php
@@ -12,13 +12,13 @@ $photo = explode(',',$xml);
 $photodecoded = base64_decode($photo[1]);
 $stickercoded = base64_decode($photo[3]);
 
-unlink("media/test".$_SESSION['user_loged_in'].$_SESSION['img_time_stamp'].".png");
+unlink("../media/test".$_SESSION['user_loged_in'].$_SESSION['img_time_stamp'].".png");
 $_SESSION['img_time_stamp'] = time();
-file_put_contents("media/test".$_SESSION['user_loged_in'].$_SESSION['img_time_stamp'].".png", $photodecoded);
-file_put_contents("stickers/temp.png", $stickercoded);
+file_put_contents("../media/test".$_SESSION['user_loged_in'].$_SESSION['img_time_stamp'].".png", $photodecoded);
+file_put_contents("../stickers/temp.png", $stickercoded);
 
-$dest = imagecreatefrompng("media/test".$_SESSION['user_loged_in'].$_SESSION['img_time_stamp'].".png");
-$src = imagecreatefrompng('stickers/temp.png');
+$dest = imagecreatefrompng("../media/test".$_SESSION['user_loged_in'].$_SESSION['img_time_stamp'].".png");
+$src = imagecreatefrompng('../stickers/temp.png');
 
 $width = ImageSx($src);
 $height = ImageSy($src);
@@ -28,10 +28,10 @@ $y = $height;
 
 ImageCopyResampled($dest,$src,0,0,0,0,$x,$y,$width,$height);
 
-header('Content-Type: image/jpeg');
-imagepng($dest, "media/test".$_SESSION['user_loged_in'].$_SESSION['img_time_stamp'].".png");
+// header('Content-Type: image/jpeg');
+imagepng($dest, "../media/test".$_SESSION['user_loged_in'].$_SESSION['img_time_stamp'].".png");
 
 imagedestroy($dest);
 imagedestroy($src);
-unlink("stickers/temp.png");
+unlink("../stickers/temp.png");
 ?>
